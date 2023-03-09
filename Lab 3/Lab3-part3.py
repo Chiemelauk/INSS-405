49b08f2dfba642db8f3beec1e354d23d931fdcb1
Chi Emela <KALUCHIEMELA97@GMAIL.COM>
2023-03-08 19:23:03 -0500

    Lab 3

---
+++ Lab 3/Lab3-part3.py
@@ -0,0 +1,8 @@
+num = input ('Enter Input num')
+print('num')
+
+if num % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
---
+++ Lab 3/Lab3.py
@@ -0,0 +1,17 @@
+#>=50 -Hot
+#30-50 -Warm
+#0-30 -Cold
+#<0 -Extreme cold
+
+
+temperature=input('Enter Temperature:')
+if(float(temperature) >=50):
+    print('Hot')
+elif (float(temperature) >=30 and float (temperature)<50):
+    print('Warm')
+elif (float(temperature) >=0 and float (temperature)<30):
+    print('Cold')
+elif (float(temperature) <-20):
+    print('Extreme chill cold, shelter in place')
+else:
+    print('Extreme Cold')
